64403e01e5d75dd02687ce77c5f9b5458f051fa6
Evyn <baconz@disroot.org>
2021-10-03 16:32:05 -0700

    it does the dew

--- Package Manager/Credits.vb
+++
@@ -1,6 +0,0 @@
-Public NotInheritable Class Credits
-    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Me.Close()
-    End Sub
-
-End Class
